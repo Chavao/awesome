474107c83da1d932b4e41bfa1512e3769b34393f
Diego Chavão <fale@chavao.net>
2023-08-31 16:36:55 -0300

    Fix themes_path using local theme folder

--- rc.lua
+++ rc.lua
@@ -49,8 +49,9 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
--- beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
-beautiful.init("/home/chavao/.config/awesome/themes/default/theme.lua")
+local gfs = require("gears.filesystem")
+local themes_path = gfs.get_dir("config").."themes/"
+beautiful.init(themes_path.."default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 -- terminal = "x-terminal-emulator"
