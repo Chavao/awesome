415c6bac414521668b15cebb460d040fd05c5d1c
Diego Chavão <fale@chavao.net>
2023-09-15 14:22:11 -0300

    Open git-cola in floating client

--- rc.lua
+++ rc.lua
@@ -538,7 +538,8 @@ awful.rules.rules = {
           "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
           "Wpa_gui",
           "veromix",
-          "xtightvncviewer"},
+          "xtightvncviewer",
+          "cola"},
 
         -- Note that the name property shown in xprop might be set slightly after creation of the client
         -- and the name shown there might not match defined rules here.
