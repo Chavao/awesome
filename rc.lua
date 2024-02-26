2c4498c0af041c6dfc6ec7c63c561f1a6fdf419d
Diego Chavão <fale@chavao.net>
2024-02-26 08:59:52 -0300

    Open calculator in floating client

--- rc.lua
+++ rc.lua
@@ -575,7 +575,8 @@ awful.rules.rules = {
           "veromix",
           "xtightvncviewer",
           "cola",
-          "MPlayer"},
+          "MPlayer",
+          "calculator"},
 
         -- Note that the name property shown in xprop might be set slightly after creation of the client
         -- and the name shown there might not match defined rules here.
