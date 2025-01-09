99038b63cbacb9e4ea1a43c651f4282703eefc5b
Diego Chavão <diego.chavao@dc3solucoes.com.br>
2025-01-09 19:21:52 -0300

    Remove Spotify to open in floating client

--- rc.lua
+++ rc.lua
@@ -604,8 +604,7 @@ awful.rules.rules = {
           "xtightvncviewer",
           "cola",
           "MPlayer",
-          "calculator",
-          "Spotify"},
+          "calculator"},
 
         -- Note that the name property shown in xprop might be set slightly after creation of the client
         -- and the name shown there might not match defined rules here.
