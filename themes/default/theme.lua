4e326fa7b972fcd38de770c5272f40f98a721e98
Diego Chavão <diego.chavao@dc3solucoes.com.br>
2025-04-30 08:44:16 -0300

    Add gap between clients

--- themes/default/theme.lua
+++ themes/default/theme.lua
@@ -24,10 +24,10 @@ theme.fg_focus      = "#ffffff"
 theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
-theme.useless_gap   = dpi(0)
-theme.border_width  = dpi(1)
-theme.border_normal = "#000000"
-theme.border_focus  = "#333333"
+theme.useless_gap   = dpi(2)
+theme.border_width  = dpi(2)
+theme.border_normal = "#2A2A2A"
+theme.border_focus  = "#4C4C4C"
 theme.border_marked = "#91231c"
 
 -- There are other variable sets
